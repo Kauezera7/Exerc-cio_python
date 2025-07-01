e4a047414bab22e3e89e96c3b920f9dfba1ff3e9
Kauezera7 <krc.omega@gmail.com>
2025-07-01 13:37:55 -0300

    meu python

--- README.md/ex013.py
+++ README.md/ex013.py
@@ -2,4 +2,3 @@ sal = float(input('Qual é o salário do funcionário? R$'))
 aum = float(input('Qual é a porcentagem de aumento?'))
 didi = sal + ((sal * aum) / 100)
 print ('O funcionário que ganhava R${}, com {:.0f}% de aumento, passo a receber R${:.2f}.'.format(sal, aum, didi))
-4
