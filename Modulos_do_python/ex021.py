1b14411ed62a72624c3f84e378f556a8f5cc5422
Kaue Rosa <krc.oemga@gmail.com>
2025-07-03 20:28:43 -0300

    meu python

--- Modulos_do_python/ex021.py
+++ Modulos_do_python/ex021.py
@@ -1,5 +1,7 @@
 import pygame
 pygame.init()
-pygame.mixer.music.load('ex021.mp3')
+pygame.mixer.music.load('Modulos_do_python/ex021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
+input("Pressione Enter para parar a música...")
+pygame.mixer.music.stop()
