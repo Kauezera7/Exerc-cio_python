fbc16456049cf59cc8219802cd4e0a8078fce079
Kauezera7 <krc.omega@gmail.com>
2025-07-15 17:19:47 -0300

    meu python

---
+++ Manipulando_Cadeias_de_texto/ex024.py
@@ -0,0 +1,2 @@
+tre = input('Digite em que cidade voce nasceu: ').strip()
+print(tre[:5].upper() == 'SANTO')       
---
+++ Manipulando_Cadeias_de_texto/ex025.py
@@ -0,0 +1,2 @@
+tre = input('Digite seu nome completo: ').strip()
+print(f'Seu nome tem Silva? {"SILVA" in tre.upper()}') 
